f50859234f75b2ffd799c61b24ce0ccc15047e3c
Aarav Singh <imaaravsingh777@gmail.com>
2025-08-24 09:40:10 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -34,7 +34,6 @@ function Settings() {
     <div className="flex h-screen bg-background text-foreground overflow-hidden">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        {/* @ts-ignore */}
         <Header />
         <main className="flex-1 overflow-y-auto p-4 sm:p-6 md:p-8 space-y-6">
           <Card>
