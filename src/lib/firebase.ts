18c66ababb6dbb26dabd069d60cf5f8eb88d9a7d
Aarav Singh <imaaravsingh777@gmail.com>
2025-08-24 09:30:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "budgetwise-f7166",
-  "appId": "1:223510455912:web:65ddea621d99b1de5f7ddd",
-  "storageBucket": "budgetwise-f7166.appspot.com",
-  "apiKey": "AIzaSyDhSHNa3w8uLJ6hkIYlfEWKDgjWE7ZrluE",
-  "authDomain": "budgetwise-f7166.firebaseapp.com",
-  "messagingSenderId": "223510455912",
-  "measurementId": "G-11V6221W4G"
+  projectId: 'budgetwise-f7166',
+  appId: '1:223510455912:web:65ddea621d99b1de5f7ddd',
+  storageBucket: 'budgetwise-f7166.appspot.com',
+  apiKey: 'AIzaSyDhSHNa3w8uLJ6hkIYlfEWKDgjWE7ZrluE',
+  authDomain: 'budgetwise-f7166.firebaseapp.com',
+  messagingSenderId: '223510455912',
 };
-
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
